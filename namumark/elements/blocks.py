2d7ccf2402ec9d8286a6bb536c7bd520e93dbf9d
Jonghwan Hyeon <hyeon0145@gmail.com>
2018-07-02 02:26:12 +0900

    Add some block elements and their specifications

--- namumark/elements/blocks.py
+++ namumark/elements/blocks.py
@@ -7,3 +7,30 @@ class Block(Element):
 
         # Blocks are normally open at first
         self.closed = False
+
+
+class Document(Block):
+    pass
+
+
+class Heading(Block):
+    def __init__(self, level):
+        super().__init__()
+
+        self.level = level
+        self.closed = True
+
+
+class Quote(Block):
+    pass
+
+
+class Paragraph(Block):
+    pass
+
+
+class ThematicBreak(Block):
+    def __init__(self):
+        super().__init__()
+
+        self.closed = True
---
+++ namumark/specs/specs.py
@@ -0,0 +1,155 @@
+import re
+
+from collections import namedtuple
+
+from . import spec_for
+from ..elements import *
+
+
+@spec_for(Block)
+class BlockSpec:
+    accepts_text = False
+
+    @classmethod
+    def create(cls, text):
+        """Try to create an element from a given text. Normally, this function
+        looks only markers and uses only those markers from the given text.
+
+        If success, returns (Created element, remaining text)
+        Otherwise, returns (None, the given text)
+        """
+        return (None, text)
+
+    @classmethod
+    def consume(cls, text):
+        """Try to consume markers from a given text. This function checks
+        whether the given text is sutiable for this element or not.
+
+        If success, returns (True, remaining text)
+        Otherwise, returns (False, the given text)
+        """
+        return (False, text)
+
+    @staticmethod
+    def can_contain(element):
+        """Returns whether the element of this specification
+        can contain a given element?
+        """
+        return False
+
+
+@spec_for(Document)
+class DocumentSpec(BlockSpec):
+    accepts_text = False
+
+    @classmethod
+    def consume(cls, text):
+        return (True, text)
+
+    @staticmethod
+    def can_contain(element):
+        return False
+
+
+@spec_for(Heading)
+class HeadingSpec(BlockSpec):
+    '''
+    = heading 1 =
+    == heading 2 ==
+    === heading 3 ===
+    ==== heading 4 ====
+    ===== heading 5 =====
+    ====== heading 6 ======
+    '''
+    syntax = re.compile(r'''
+        ^
+            (\={1,6})  # marker
+                [ ]+  # required whitespace
+                    (.*)  # text
+                [ ]+  # required whitespace
+            \1  # marker
+        $
+    ''', re.VERBOSE)
+
+    accepts_text = True
+
+    @classmethod
+    def create(cls, text):
+        match = cls.syntax.search(text)
+        if not match:
+            return (None, text)
+
+        level = len(match.group(1))
+        return (Heading(level=level), match.group(2))
+
+    @staticmethod
+    def can_contain(element):
+        return False
+
+
+@spec_for(Quote)
+class QuoteSpec(BlockSpec):
+    '''
+    > text 1
+    > text 2
+    '''
+    syntax = re.compile(r'''
+        ^
+            \>  # marker
+                [ ]*  # optional whitespace
+                    (.*)  # text
+        $
+    ''', re.VERBOSE)
+
+    aceepts_text = False
+
+    @classmethod
+    def create(cls, text):
+        consumed, remaining_text = cls.consume(text)
+        if not consumed:
+            return (None, text)
+
+        return (Quote(), remaining_text)
+
+    @classmethod
+    def consume(cls, text):
+        match = cls.syntax.search(text)
+        if not match:
+            return (False, text)
+
+        return (True, match.group(1))
+
+    @staticmethod
+    def can_contain(element):
+        return True
+
+
+@spec_for(ThematicBreak)
+class ThematicBreakSpec(BlockSpec):
+    '''
+    ----
+    -----
+    ------
+    -------
+    --------
+    ---------
+    '''
+    syntax = re.compile(r'''
+        ^
+            \-{4,9}  # marker
+        $
+    ''', re.VERBOSE)
+
+    aceepts_text = False
+
+    @classmethod
+    def create(cls, text):
+        match = cls.syntax.search(text)
+        if not match:
+            return (None, text)
+
+        return (ThematicBreak(), '')
+
+    @staticmethod
+    def can_contain(element):
+        return False
